9205abf73a31194bacc9fb2c5070b4624a1bebb9
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-11-29 12:24:06 -0300

    install pg and gambiarra on patch update posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index show edit destroy new create] do
     resources :comments, only: [:create]
   end
+  patch '/posts/:id', to: 'posts#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end

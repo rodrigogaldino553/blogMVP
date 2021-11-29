d94422ce83a95d813e575064b7f25924054f2f5a
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-11-28 22:24:24 -0300

    adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    # create a comment
+    # for the post from the current admin
+    @comment = current_admin.comments.new(comment_params)
+    if !@comment.save
+      flash[:notice] = @comment.errors.full_messages.to_sentence
+    end
+
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+
+  def comment_params
+    params
+      .require(:comment)
+      .permit(:content)
+      .merge(post_id: params[:post_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   # resources :posts # add only[%sei la oq%]
-  resources :posts, only: %i[index show edit destroy new create]
+  resources :posts, only: %i[index show edit destroy new create] do
+    resources :comments, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end

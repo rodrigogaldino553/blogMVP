99f3ea0b7d589b7c219f040ea10f16a99c683c0a
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-12-03 21:53:42 -0300

    install bootstrap by yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})

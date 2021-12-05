099952d62da617a1dcc28f2d9b1a1048b9972041
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2021-12-05 10:58:55 -0300

    fix bootstrap, add footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-document.addEventListener("turbolinks:load", () => {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-})
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
+
+import "@fortawesome/fontawesome-free/css/all"
